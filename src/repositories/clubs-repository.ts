d5fcfa4792951ae3fbac8f47629b7f206e54c4e5
Raphael R <reis.rrsc@outlook.com>
2026-01-09 07:30:36 -0300

    leitura de arquivo json

--- src/repositories/clubs-repository.ts
+++ src/repositories/clubs-repository.ts
@@ -1,9 +1,13 @@
 import { ClubModel } from "../models/club-model";
+import fs from 'fs/promises';
 
 const database = [
     {id: 1, name: "Club A"}
 ]
 
 export const findAllClubs = async (): Promise<ClubModel[]> => {
-    return database;
+    const data = await fs.readFile('./src/data/clubs.json', 'utf-8');
+    const clubs: ClubModel[] = JSON.parse(data);
+
+    return clubs;    
 };
